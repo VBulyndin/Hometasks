811fc16c88cfbacfd3d8085306b2c6b236d51609
vbulyndin <vbulyndin@gmail.com>
2016-03-26 16:14:20 +0200

    added JS_3_4 HomeTasks

--- JS.Module_1_2_2/js/script.js
+++ JS.Module_1_2_2/js/script.js
@@ -1,15 +1,17 @@
-var array = [];
-for (var i = 0; i < 5; i++) {
-  array[i] = prompt('Ваше имя?','');
-}
-console.log('array', array);
-var userLog = prompt('Введите имя пользователя для авторизации:','');
-for (var i = 0; i < 5; i++) {
-  if (userLog === array[i]) {
-    var user = array[i];
-    alert (userLog + ', Вы успешно авторизировались');
-  }else if (user === undefined) {
-    alert (userLog + ', Вам отказано в доступе');
-    break;
-  }
-}
+// var array = [];
+// for (var i = 0; i < 5; i++) {
+//   array[i] = prompt('Ваше имя?','');
+// }
+// console.log('array', array);
+// var userLog = prompt('Введите имя пользователя для авторизации:','');
+// for (var i = 0; i < 5; i++) {
+//   if (userLog === array[i]) {
+//     var user = array[i];
+//     alert (userLog + ', Вы успешно авторизировались');
+//   }else if (user === undefined) {
+//     alert (userLog + ', Вам отказано в доступе');
+//     break;
+//   }
+// }
+var result = 5 + '5' - 0;
+console.log(result);
